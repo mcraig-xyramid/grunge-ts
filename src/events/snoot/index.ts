06f0caacbed012be448d63b06b93aceb4710fa4f
Michael Craig <michael@michaelcraig.group>
2023-11-06 10:44:58 -0700

    lint & ts fixes

--- src/events/snoot/index.ts
+++ src/events/snoot/index.ts
@@ -1,8 +1,8 @@
-let arc = require('@architect/functions')
+import arc from '@architect/functions'
 
 exports.handler = arc.events.subscribe(handler)
 
-async function handler (event) {
+async function handler (event: string) {
   console.log(event)
   return
 }
