a53aa9afe26debf0ee8446a9c1b1db91d5136fc8
Michael Craig <michael@michaelcraig.group>
2023-11-06 11:31:30 -0700

    type fix

--- src/events/snoot/index.ts
+++ src/events/snoot/index.ts
@@ -1,6 +1,4 @@
-import arc from '@architect/functions'
-
-export const handler = async (event) => {
+export const handler = async (event: string) => {
     console.log("Papyrus incoming default handler called with", event);
   
 }
